1e39bcbec8910c804b8e2d366ce03bccbafbc17e
suansuan <wysujhu@gmail.com>
2024-01-15 16:45:10 -0500

    Completed password reset functionality.
    Also correct a typo in EqualTo().

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -68,5 +68,5 @@ class RequestResetForm(FlaskForm):
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators = [DataRequired()])
     confirm_password = PasswordField('Confirm Password', 
-                                    validators = [DataRequired(), EqualTo(password)])
+                                    validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
